5e6282e31db2b5913664ab75e31541530d6c7c68
meniAvitan <66748984+meniAvitan@users.noreply.github.com>
2022-03-10 00:26:34 +0200

    continue with add property form

--- frontend/src/app/components/property/add-property/add-property.component.ts
+++ frontend/src/app/components/property/add-property/add-property.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-add-property',
@@ -9,6 +10,11 @@ import { Router } from '@angular/router';
 })
 export class AddPropertyComponent implements OnInit {
   @ViewChild('Form') AddPropertyForm: NgForm;
+  @ViewChild('formTabs') formTabs?: TabsetComponent;
+
+  propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
+  fornishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
+  directionTypes: Array<string> = ['East', 'West', 'South', 'North'];
 
 
   constructor(private router: Router) { }
@@ -24,9 +30,11 @@ export class AddPropertyComponent implements OnInit {
   onSubmit(){
 
     console.log(this.AddPropertyForm);
-
-
-
+  }
+  selectTab(tabId: number) {
+    if (this.formTabs?.tabs[tabId]) {
+      this.formTabs.tabs[tabId].active = true;
+    }
   }
 
 }
